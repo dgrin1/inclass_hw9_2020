d78b7a20e1e84536e9a5fdc66db7a5fb7b3cf6f4
dgrin1 <dgrin314@gmail.com>
2020-03-18 14:09:41 -0400

    added commented version of coupled system

--- compare.py
+++ compare.py
@@ -11,39 +11,45 @@ N = 1000          # Number of steps
 h = (b-a)/N       # Size of a single step
 x = 0.0           # Initial condition
 
+#Euler's method using -- using slope at one point
 tpoints = arange(a,b,h)
 xpoints = []
 for t in tpoints:
     xpoints.append(x)
     x += h*f(x,t)
-
-plot(tpoints,xpoints,'k')
+#plot right here
+plot(tpoints,xpoints,'ko')
 xlabel("t")
 ylabel("x(t)")
 
+#set same region with smaller number points
 a = 0.0
 b = 10.0
 N = 100
 h = (b-a)/N
 
+#create list of points rranged by step size
 tpoints = arange(a,b,h)
 x2points = []
 
 x = 0.0
+#loop over time
 for t in tpoints:
     x2points.append(x)
+ #two estimates of slope, used to compute average slope and take a better step
     k1 = h*f(x,t)
     k2 = h*f(x+0.5*k1,t+0.5*h)
     x += k2
-
+#plot better estimate
 plot(tpoints,x2points,'r--')
 
 
 
-
+#runge kutta 4
 
 a = 0.0
 b = 10.0
+#I can do just as well, using only 40 points
 N = 40
 h = (b-a)/N
 
@@ -53,13 +59,14 @@ x = 0.0
 
 for t in tpoints:
     x4points.append(x)
+#4 estimates of the step
     k1 = h*f(x,t)
     k2 = h*f(x+0.5*k1,t+0.5*h)
     k3 = h*f(x+0.5*k2,t+0.5*h)
     k4 = h*f(x+k3,t+h)
     x += (k1+2*k2+2*k3+k4)/6
 
-plot(tpoints,x4points,'m')
+plot(tpoints,x4points,'m*')
 
 
 
